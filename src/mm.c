ed16a3412afb7f700ead06278393035a3b3dcc21
b rad c <bradcorlett@gmail.com>
2025-04-19 18:08:24 -0700

    remove unused variables, add cli menu

--- src/mm.c
+++ src/mm.c
@@ -1,19 +1,5 @@
 /* mm.c -- midi monitor */
 
-/*****************************************************************************
-*       Change Log
-*  Date | Change
-*-----------+-----------------------------------------------------------------
-*  7-Apr-86 | Created changelog
-* 31-Jan-90 | GWL : use new cmdline
-*  5-Apr-91 | JDW : Further changes
-* 16-Feb-92 | GWL : eliminate label mmexit:; add error recovery
-* 18-May-92 | GWL : continuous clocks, etc.
-* 17-Jan-94 | GWL : option to display notes
-* 20-Nov-06 | RBD : port mm.c from CMU Midi Toolkit to PortMidi
-*           |       mm.c -- revealing MIDI secrets for over 20 years!
-*****************************************************************************/
-
 #include "stdlib.h"
 #include "ctype.h"
 #include "string.h"
@@ -23,42 +9,11 @@
 #include "signal.h"
 #include <unistd.h>
 
-#define STRING_MAX 80
 
 #define MIDI_CODE_MASK  0xf0
 #define MIDI_CHN_MASK   0x0f
-/*#define MIDI_REALTIME   0xf8
-  #define MIDI_CHAN_MODE  0xfa */
-#define MIDI_OFF_NOTE   0x80
-#define MIDI_ON_NOTE    0x90
-#define MIDI_POLY_TOUCH 0xa0
 #define MIDI_CTRL       0xb0
-#define MIDI_CH_PROGRAM 0xc0
-#define MIDI_TOUCH      0xd0
-#define MIDI_BEND       0xe0
-
-#define MIDI_SYSEX      0xf0
-#define MIDI_Q_FRAME	0xf1
-#define MIDI_SONG_POINTER 0xf2
-#define MIDI_SONG_SELECT 0xf3
-#define MIDI_TUNE_REQ	0xf6
-#define MIDI_EOX        0xf7
-#define MIDI_TIME_CLOCK 0xf8
-#define MIDI_START      0xfa
-#define MIDI_CONTINUE	0xfb
-#define MIDI_STOP       0xfc
-#define MIDI_ACTIVE_SENSING 0xfe
-#define MIDI_SYS_RESET  0xff
-
 #define MIDI_ALL_SOUND_OFF 0x78
-#define MIDI_RESET_CONTROLLERS 0x79
-#define MIDI_LOCAL	0x7a
-#define MIDI_ALL_OFF	0x7b
-#define MIDI_OMNI_OFF	0x7c
-#define MIDI_OMNI_ON	0x7d
-#define MIDI_MONO_ON	0x7e
-#define MIDI_POLY_ON	0x7f
-
 
 #define private static
 
@@ -72,27 +27,8 @@ typedef int boolean;
 int debug = false;	/* never set, but referenced by userio.c */
 PmStream *midi_in;      /* midi input */
 boolean active = false;     /* set when midi_in is ready for reading */
-boolean in_sysex = false;   /* we are reading a sysex message */
 volatile sig_atomic_t done = 0;       /* when non zero, exit */;
-boolean notes = true;       /* show notes? */
-boolean controls = true;    /* show continuous controllers */
-boolean bender = true;      /* record pitch bend etc.? */
-boolean excldata = true;    /* record system exclusive data? */
-boolean verbose = true;     /* show text representation? */
-boolean realdata = true;    /* record real time messages? */
-boolean clksencnt = true;   /* clock and active sense count on */
-boolean chmode = true;      /* show channel mode messages */
-boolean pgchanges = true;   /* show program changes */
-boolean flush = false;	    /* flush all pending MIDI data */
 
-uint32_t filter = 0;            /* remember state of midi filter */
-
-uint32_t clockcount = 0;        /* count of clocks */
-uint32_t actsensecount = 0;     /* cout of active sensing bytes */
-uint32_t notescount = 0;        /* #notes since last request */
-uint32_t notestotal = 0;        /* total #notes */
-
-char val_format[] = "    Val %d\n";
 
 /*****************************************************************************
 *    Imported variables
@@ -104,14 +40,14 @@ extern  int     abort_flag;
 *    Routines local to this module
 *****************************************************************************/
 
-private    void    mmexit(int code);
-private    void    output(PmMessage data);
-private    int     get_number(const char *prompt);
-
-char *nanoKONTROL2_getControlName(int control);
+private    void    handle_midi_event(PmMessage data);
+private    int     get_user_input_as_number(const char *prompt);
 
 
-int get_number(const char *prompt) {
+char *nano_kontrol2_control_name(int control);
+
+
+int get_user_input_as_number(const char *prompt) {
     /* read a number from console */
     int n = 0, i;
     fputs(prompt, stdout);
@@ -123,19 +59,31 @@ int get_number(const char *prompt) {
 }
 
 
-void receive_poll(PtTimestamp timestamp, void *userData) {
+void poll_midi_device(PtTimestamp timestamp, void *userData) {
     PmEvent event;
     int count;
     if (!active) return;
     while ((count = Pm_Read(midi_in, &event, 1))) {
-        if (count == 1) output(event.message);
+        if (count == 1) handle_midi_event(event.message);
         else            puts(Pm_GetErrorText(count));
     }
 }
 
+
 void interrupt_handler(int dummy) {
     done = 1;
     printf("Caught signal %d\n", dummy);
+}
+
+void help_menu(int exit_code) {
+    puts("Usage: mm [run|list] [--device <device_name>]");
+    puts("Commands:");
+    puts("  run                     Start the MIDI monitor.");
+    puts("  list                    List available MIDI devices.");
+    puts("Options:");
+    puts("  --device <device_name>  Specify the MIDI device name to use. Default: 'nanoKONTROL2 nanoKONTROL2 _ CTR'");
+    puts("  -h, --help              Show this help message.");
+    exit(exit_code);
 }
 
 
@@ -143,95 +91,126 @@ void interrupt_handler(int dummy) {
 *               main
 ****************************************************************************/
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
     PmError err;
     int i;
-    int preferred = -1;
-    int indexToUse;
+    char *command;
+    char *device_name = "nanoKONTROL2 nanoKONTROL2 _ CTR";
+    int device_index = -1;
 
-    /* select input device */
-    
-    Pt_Start(1, receive_poll, 0);
-    puts("MIDI input devices:");
-    for (i = 0; i < Pm_CountDevices(); i++) {
-        const PmDeviceInfo *info = Pm_GetDeviceInfo(i);
-        if (info->input) printf("%d: %s, '%s'\n", i, info->interf, info->name);
-        if (strcmp(info->name, "nanoKONTROL2 nanoKONTROL2 _ CTR") == 0) {
-            preferred = i;
+    /*
+    parse cli arguments
+    */
+
+
+    /* get command */
+    if (argc > 1) {
+        if (strcmp(argv[1], "run") == 0) {
+            command = "run";
+        } else if (strcmp(argv[1], "list") == 0) {
+            command = "list";
+        } else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            help_menu(0);
+        } else {
+            puts("Unknown command. Exiting.");
+            help_menu(1);
+        }
+    }else {
+        puts("No command provided. Exiting.");
+        help_menu(1);
+    }
+
+    /* get options */
+    if (argc > 2) {
+        if (strcmp(argv[2], "--device") == 0) {
+            if (argc > 3) {
+                device_name = argv[3];
+            } else {
+                puts("Did not provide device name. Exiting.");
+                help_menu(1);
+            }
+        } else if (strcmp(argv[2], "-h") == 0 || strcmp(argv[2], "--help") == 0) {
+            help_menu(0);
+        } else {
+            puts("Unknown option. Exiting.");
+            help_menu(1);
         }
     }
+
+    /* 
+    select / list input device 
+    */
     
-    if (preferred != -1) {
-        indexToUse = preferred;
-        printf("Using preferred device: %d\n", preferred);
-    } else {
-        indexToUse = get_number("Type input device number: ");
+    Pt_Start(1, poll_midi_device, 0);
+    if (strcmp(command, "list") == 0) puts("MIDI input devices:");
+
+    for (i = 0; i < Pm_CountDevices(); i++) {
+        const PmDeviceInfo *info = Pm_GetDeviceInfo(i);
+
+        if (strcmp(command, "list") == 0 && info->input) printf("  %d) '%s'\n", i, info->name);
+
+        if (strcmp(info->name, device_name) == 0) {
+            device_index = i;
+        }
     }
 
-    /* init midi */
+    if (strcmp(command, "list") == 0) {
+        Pt_Stop();
+        exit(0);
+    }
+    
+    if (device_index == -1) {
+        printf("Could not find device '%s'.\n", device_name);
+        exit(1);
+    }
 
-    err = Pm_OpenInput(&midi_in, indexToUse, NULL, 512, NULL, NULL);
+    /* 
+    init midi 
+    */
+
+    err = Pm_OpenInput(&midi_in, device_index, NULL, 512, NULL, NULL);
     if (err) {
         puts(Pm_GetErrorText(err));
         Pt_Stop();
-        mmexit(1);
+        exit(1);
     }
 
-    printf("Midi device %d opened.\n", indexToUse);
+    printf("Midi device opened: %s\n", device_name);
 
-    Pm_SetFilter(midi_in, filter);
     printf("Midi Monitor ready. (pid: %d) (Control+C to exit)\n", getpid());
     active = true;
 
-    /* main loop */
+    /* 
+    main loop 
+    */
 
     signal(SIGINT, interrupt_handler);
     signal(SIGTERM, interrupt_handler);
 
     while (!done) {}
 
-    /* clean up and exit */
+    /* 
+    clean up and exit 
+    */
 
     printf("Midi Monitor exiting.\n");
     active = false;
     Pm_Close(midi_in);
     Pt_Stop();
     Pm_Terminate();
-    mmexit(0);
-    return 0; // make the compiler happy be returning a value
+
+    return 0;
+
 }
 
 
+private void handle_midi_event(PmMessage data) {
 
-private void mmexit(int code)
-{
-    /* if this is not being run from a console, maybe we should wait for
-     * the user to read error messages before exiting
-     */
-    exit(code);
-}
-
-
-/****************************************************************************
-*               output
-* Inputs:
-*    data: midi message buffer holding one command or 4 bytes of sysex msg
-* Effect: format and print  midi data
-****************************************************************************/
-
-char vel_format[] = "    Vel %d\n";
-
-private void output(PmMessage data)
-{
     int command;    /* the current command */
     int chan;   /* the midi channel of the current event */
     int control; /* the controller number of the current event */
     int value;  /* the controller value of the current event */
-    // int len;    /* used to get constant field width */
-
-    /* printf("output data %8x; ", data); */
 
     command = Pm_MessageStatus(data) & MIDI_CODE_MASK;
     chan = Pm_MessageStatus(data) & MIDI_CHN_MASK;
@@ -239,16 +218,14 @@ private void output(PmMessage data)
     value = Pm_MessageData2(data);
 
     if (command == MIDI_CTRL) {
-        /* controls 121 (MIDI_RESET_CONTROLLER) to 127 are channel
-         * mode messages. */
         if (Pm_MessageData1(data) < MIDI_ALL_SOUND_OFF) {
-            printf("%s Chan %2d Ctrl %2d Val %2d\n", nanoKONTROL2_getControlName(control), chan, control, value);
+            printf("%s Chan %2d Ctrl %2d Val %2d\n", nano_kontrol2_control_name(control), chan, control, value);
         }
     }
     fflush(stdout);
 }
 
-char* nanoKONTROL2_getControlName(int control) {
+char* nano_kontrol2_control_name(int control) {
     switch (control) {
         case 0: return "fader1";
         case 1: return "fader2";
